a58643e0c3fb62b320b729d1a4c4a32a4e28d3d3
Daniel <danielgibson.pe@gmail.com>
2025-09-08 13:33:40 +0200

    Alternative actions and Selectors, v694

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
+import { decrement, increment } from '../store/counter.actions';
 
 
 
@@ -18,6 +18,6 @@ export class CounterControlsComponent {
   }
 
   decrement() {
-    
+    this.store.dispatch(decrement({ value: 1 }))
   }
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,13 +1,18 @@
-import { createReducer, on } from "@ngrx/store";
-import { increment } from "./counter.actions";
+import { Action, createReducer, on } from "@ngrx/store";
+//import { CounterActions, IncrementAction } from "./counter.actions";
+import { decrement, increment } from "./counter.actions";
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
     initialState,
-    on(increment, (state, action) => state + action.value)
+    on(increment, (state, action) => state + action.value),
+    on(decrement, (state, action) => state - action.value)
 );
 
-// export function counterReducer(state = initialState) {
+// export function counterReducer(state = initialState, action: CounterActions | Action) {
+//     if (action.type === '[Counter] Increment' ) {
+//         return state + (action as IncrementAction).value;
+//     }
 //     return state;
 // }
